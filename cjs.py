3479b6a9f4b1a99d27825e8c14d90f7e43e46ea6
CJSoft <egwcyh@qq.com>
2015-09-09 22:07:29 +0800

    2015/09/09 周三 22:07:28.89

--- cjs.py
+++ cjs.py
@@ -3,12 +3,12 @@ import os,json
 class dumb(object):
 	def __init__(self):
 		vcap_services=json.loads(os.getenv("VCAP_SERVICES"))
-		sqlservices=vcap_services["mysql"][0]
-		self.name=sqlservices["credentials"]["name"]
-		self.hostname=sqlservices["credentials"]["hostname"]
-		self.port=sqlservices["credentials"]["port"]
-		self.username=sqlservices["credentials"]["username"]
-		self.password=sqlservices["credentials"]["password"]
-		self.uri=sqlservices["credentials"]["uri"]
-		self.jdbcUrl=sqlservices["credentials"]["jdbcUrl"]
+		sqlservices=vcap_services[u"mysql"][0]
+		self.name=sqlservices[u"credentials"][u"name"]
+		self.hostname=sqlservices[u"credentials"][u"hostname"]
+		self.port=sqlservices[u"credentials"][u"port"]
+		self.username=sqlservices[u"credentials"][u"username"]
+		self.password=sqlservices[u"credentials"][u"password"]
+		self.uri=sqlservices[u"credentials"][u"uri"]
+		self.jdbcUrl=sqlservices[u"credentials"][u"jdbcUrl"]
 sql=dumb()
